2132a5a4140e404db6872e647fc1c4a7496034d8
Arun solanki <arunsolanki@excellencetechnologies.in>
2022-02-25 12:16:28 +0530

    firebase

--- src/screens/CreatePost.js
+++ src/screens/CreatePost.js
@@ -1,11 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import storage from '@react-native-firebase/storage';
 
 const CreatePost = () => {
+  const reference = storage().ref('images');
+
+  const storeImage = async () =>{
+    console.log("imagestore.png")
+    await reference.putFile("C:\Users\Etech\Downloads\frame.png/imagestore.png");
+  }
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.signUpOnLogin}>
+      <TouchableOpacity style={styles.signUpOnLogin} onPress={storeImage}>
           <Text style={styles.signUpOnLoginText}>Create Post</Text>
       </TouchableOpacity>
     </View>
